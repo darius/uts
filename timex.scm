2529c58e04a0291f0eff37a42ecad3edaffcc467
Darius Bacon <withal@gmail.com>
2026-03-14 08:17:31 -0500

    rm unused

--- timex.scm
+++
@@ -1,9 +0,0 @@
-;; Return (k elapsed (proc))
-;; where elapsed is the time in seconds it took to evaluate (proc).
-
-(define timex
-  (lambda (proc k)
-    (let ((start (@runtime)))
-      (let ((result (proc)))
-	(k (- (@runtime) start)
-	   result)))))
